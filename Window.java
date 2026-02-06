81ef6bf7e2596e974bfc1ce644cbc2faee0888f0
Hayl Cowan <cowanh2@wwu.edu>
2026-02-05 19:34:53 -0800

    Window.java attributes and methods take 2

--- Window.java
+++ Window.java
@@ -1,10 +1,21 @@
 /*
 //	This is an Object that the UIManager can use to portray info besides the board
 */
-
+import java.awt.image.BufferedImage;
 public class Window{
+        private BufferedImage background;
+        private boolean show;
         public static void main(String[] args){
                 System.out.println("Running Window.java");
+        }
+
+        //toggles if the window is visual
+        public void toggleShow(){
+
+        }
+        //checks if the window is showing
+        public void getShow(){
+
         }
 
 }
