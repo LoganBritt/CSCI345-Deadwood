774d27e4136ade2b5197f5354931add825427f62
Logan Britt <brittm2@wwu.edu>
2026-02-05 20:22:34 -0800

    adding all

--- GameManager.java
+++ GameManager.java
@@ -3,9 +3,61 @@
 //	GameManager maintains turn order and the flow of the game
 */
 
+import java.util.Random;
+import java.util.Arrays;
+
 public class GameManager{
+	private BoardManager bm;
+	private UIManager ui;
+	private int day;
+	private Player[] playerListOrder;
+	private int playerAmt;
+	private int playerIdx = 0;
+
         public static void main(String[] args){
                 System.out.println("Running GameManager.java");
         }
+
+	//Is called to start the game
+	//Creates the objects for the game to play with
+	public void startGame(){
+		int[] diceReturn = rollDice(5);
+		System.out.println("Rolling 5 dice:");
+		System.out.println(diceReturn.toString);
+	}
+
+	public int getDay(){
+		return day;
+	}
+
+	public void changeTurn(){
+		if(playerIdx+1 == playerAmt){
+			playerIdx = 0;
+		}else{
+			playerIdx++;
+		}
+	}
+
+	public Player getActivePlayer(){
+		return playerListOrder[playerIdx];
+	}
+
+	public Player[] getPlayerList(){
+		return playerListOrder;
+	}
+
+	public void distributeBonuses(Player[] bonusPlayers, int budget){
+
+	}
+
+	public int[] rollDice(int dieAmt){
+		Random rand = new Random();
+		int[] retVals;
+		for(int i = 0; i < dieAmt; i++){
+			retVals[i] = rand.nextInt(1, 7);
+		}
+
+		return Arrays.sort(retVals);
+	}
 
 }
