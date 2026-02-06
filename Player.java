a339a6c3df03dfa2cd6152af15c48b576ad92a6e
Hayl Cowan <cowanh2@wwu.edu>
2026-02-05 18:50:00 -0800

    Player Class Implementation

--- Player.java
+++ Player.java
@@ -2,29 +2,64 @@
 //	This Class represents each of the players playing the game
 //	It contains the specific data that belongs to each player
 //	Player related actions (Acting, rehearsing moving, etc), are called from here
-*/
 
-public class Player{
-	public static void main(String[] args){
+//  TakeRole is handled in Role, not in Player
+*/
+import java.util.Random;
+public class Player {
+
+	public int dollars;
+	public int credits;
+	public int rank;
+	public int rehearseTokens;
+	public Space currLocation;
+
+	public static void main(String[] args) {
 		System.out.println("Running Player.java");
 	}
 
-	//Move: Player can move to an adjacent scene
-	
+	// Move: Player can move to an adjacent scene
+	public void move(Space newLocation) {
+		currLocation = newLocation;
+	}
 
-	//Take Role: Player can take a role of the current scene they are in
+	// Act: Player can choose to act, depending on success or fail, the player will
+	// revieve rewards for working on/off card
+	public void act(boolean onCard) {
+		Random r = new Random();
+		int roll = r.nextInt(1,7);
+		int budget = currLocation.card.getBudget();
 
-	//Act: Player can choose to act, depending on success or fail, the player will 
-	// revieve rewards for working on/off card 
-	public void act(Role role){
+		if (roll + rehearseTokens >= budget){
+			currLocation.shots--;
+			clearTokens();
+			if (onCard){
+				credits += 2;
+			}
+			else{
+				dollars++;
+				credits++;
+			}
+		}
+
+		else if (!onCard){
+			dollars++;
+		}
 
 	}
-	//Rehearse: Adds practice chip to the die, gives +1 to all die rolls 
-	public void rehearse(){
-		
-	}
-	//Upgrade: Pay $/Credits to upgrade 
 
-	
+	// Rehearse: Adds practice chip to the die, gives +1 to all die rolls
+	public void rehearse() {
+		rehearseTokens++;
+	}
+
+	// Upgrade: Pay $/Credits to upgrade
+	public void upgrade(int newRank) {
+		rank = newRank;
+	}
+
+	private void clearTokens() {
+		rehearseTokens = 0;
+	}
 
 }
