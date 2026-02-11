4b029fc1e953ed53cb8bb34e0ad881e2f52b5a2e
Hayl Cowan <cowanh2@wwu.edu>
2026-02-11 15:36:50 -0800

    Adding the Parse Class and boards/cards xml files

---
+++ Parse.java
@@ -0,0 +1,16 @@
+
+public class Parse {
+    // parseCard Method parses the cards and returns the values
+    public static Card parseCard(Card card){
+        return card;
+    }
+
+    // parseBoard method parses the board and returns the values
+    public static Board parseBoard(Board board){
+        return board;
+    }
+
+
+
+
+}
